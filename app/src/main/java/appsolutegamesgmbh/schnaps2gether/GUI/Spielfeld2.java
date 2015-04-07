ff03e3a7610b95d2910e6151e8b05500d20c8c14
frederikflpl <frederik.platter@hotmail.de>
2015-04-07 11:34:02 +0200

    GUI-Integration
    Als erste lauffähige Version gedacht. Enthält den Großteil der aktuellen
    Spielaktionen.

--- app/src/main/java/appsolutegamesgmbh/schnaps2gether/GUI/Spielfeld2.java
+++ app/src/main/java/appsolutegamesgmbh/schnaps2gether/GUI/Spielfeld2.java
@@ -3,23 +3,20 @@ package appsolutegamesgmbh.schnaps2gether.GUI;
 import android.app.DialogFragment;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
 import java.util.ArrayList;
-import java.util.Observable;
-import java.util.Observer;
 
+import appsolutegamesgmbh.schnaps2gether.DataStructure.Bummerl2;
 import appsolutegamesgmbh.schnaps2gether.DataStructure.Karte;
 import appsolutegamesgmbh.schnaps2gether.DataStructure.Spiel2;
 import appsolutegamesgmbh.schnaps2gether.DataStructure.Spieler;
 import appsolutegamesgmbh.schnaps2gether.R;
 
 
-public class Spielfeld2 extends ActionBarActivity implements View.OnClickListener, Observer {
+public class Spielfeld2 extends ActionBarActivity implements View.OnClickListener {
 
     /* TODO: Hand von Spieler1 auslesen und anzeigen; Trumpf Karte anzeigen;
     * TODO: Klick auf Karte Spieler 1
@@ -40,6 +37,7 @@ public class Spielfeld2 extends ActionBarActivity implements View.OnClickListene
     private Button buttonI;
     private Button buttonE;
     private Button buttonT;
+    private Button buttonZ;
     private Spieler s1;
     private Spieler s2;
     private Karte k1;
@@ -53,6 +51,7 @@ public class Spielfeld2 extends ActionBarActivity implements View.OnClickListene
     private TextView i;
     private TextView enemy;
     private ArrayList<Karte> stapel;
+    private Bummerl2 bummerl;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -65,14 +64,22 @@ public class Spielfeld2 extends ActionBarActivity implements View.OnClickListene
 
         button1 = (Button) findViewById(R.id.main_button1);
         button1.setOnClickListener(this);
+        button1.setClickable(true);
         button2 = (Button) findViewById(R.id.main_button2);
         button2.setOnClickListener(this);
+        button2.setClickable(true);
         button3 = (Button) findViewById(R.id.main_button3);
         button3.setOnClickListener(this);
+        button3.setClickable(true);
         button4 = (Button) findViewById(R.id.main_button4);
         button4.setOnClickListener(this);
+        button4.setClickable(true);
         button5 = (Button) findViewById(R.id.main_button5);
         button5.setOnClickListener(this);
+        button5.setClickable(true);
+        buttonZ = (Button) findViewById(R.id.main_buttonZ);
+        buttonZ.setOnClickListener(this);
+        buttonZ.setClickable(true);
         buttonI = (Button) findViewById(R.id.main_buttonI);
         buttonE = (Button) findViewById(R.id.main_buttonE);
         buttonT = (Button) findViewById(R.id.main_buttonT);
@@ -82,111 +89,124 @@ public class Spielfeld2 extends ActionBarActivity implements View.OnClickListene
         enemy = (TextView) findViewById(R.id.Enemy);
 
         buttonT.setText("20");
-    }
 
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_spielfeld, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
+        bummerl = new Bummerl2();
+        e1 = null;
+        karteGezogen();
+        zugWechsel();
     }
 
     @Override
     public void onClick(View view) {
         switch(view.getId()) {
             case R.id.main_button1:
-                String b1 = button1.getText().toString();
-                button1.setText("gespielt");
-                buttonI.setText(b1);
+                zugAusführen(k1);
+                button1.setText("");
                 break;
             case R.id.main_button2:
-                String b2 = button2.getText().toString();
-                button2.setText("gespielt");
-                buttonI.setText(b2);
+                zugAusführen(k2);
+                button2.setText("");
                 break;
             case R.id.main_button3:
-                String b3 = button3.getText().toString();
-                button3.setText("gespielt");
-                buttonI.setText(b3);
+                zugAusführen(k3);
+                button3.setText("");
                 break;
             case R.id.main_button4:
-                String b4 = button4.getText().toString();
-                button4.setText("gespielt");
-                buttonI.setText(b4);
+                zugAusführen(k4);
+                button4.setText("");
                 break;
             case R.id.main_button5:
-                String b5 = button5.getText().toString();
-                button5.setText("gespielt");
-                buttonI.setText(b5);
+                zugAusführen(k5);
+                button5.setText("");
+                break;
+            case R.id.main_buttonZ:
+                spiel.Zudrehen();
+                buttonZ.setActivated(false);
                 break;
         }
-        button1.setActivated(false);
-        button2.setActivated(false);
-        button3.setActivated(false);
-        button4.setActivated(false);
-        button5.setActivated(false);
     }
 
-    @Override
-    public void update(Observable observable, Object o) {
-        if (o.equals("Zug")) {
-            boolean temp = s1.isIstdran();
-            button1.setActivated(temp);
-            button2.setActivated(temp);
-            button3.setActivated(temp);
-            button4.setActivated(temp);
-            button5.setActivated(temp);
-            if (temp) {
-                i.setTextColor(0xff0000);
-                enemy.setTextColor(0x555555);
-            } else {
-                i.setTextColor(0x555555);
-                enemy.setTextColor(0xff0000);
+    private void zugAusführen(Karte k) {
+        if (spiel.DarfKarteAuswaehlen(e1, k)) {
+            spiel.Auspielen(k);
+            gespielteKarteEntfernen();
+            buttonI.setText(k.getFarbe()+k.getWertigkeit());
+            if (e1.equals(null)) {
+                zugWechsel();
             }
-        } else if (o.equals("Ziehen")) {
-            //stapel = spiel.getStapel();
-            buttonT.setText(stapel.size());
-            k1 = s1.Hand.get(0);
-            k2 = s1.Hand.get(1);
-            k3 = s1.Hand.get(2);
-            k4 = s1.Hand.get(3);
-            k5 = s1.Hand.get(4);
-            button1.setText(k1.getFarbe()+k1.getWertigkeit());
-            button2.setText(k2.getFarbe()+k2.getWertigkeit());
-            button3.setText(k3.getFarbe()+k3.getWertigkeit());
-            button4.setText(k4.getFarbe()+k4.getWertigkeit());
-            button5.setText(k5.getFarbe()+k5.getWertigkeit());
-        } else if (o.equals("Punkte")) {
-            buttonI.setText("");
-            buttonE.setText("");
-            int p1 = s1.getPunkte();
-            int p2 = s2.getPunkte();
-            punkte.setText(p1+":"+p2);
-        } else if (o.equals("Trumpfkarte")) {
-            //t = spiel.getAufgedeckterTrumpf();
-            buttonT.setText(t.getFarbe()+t.getWertigkeit());
-        } else if (o.equals("KI-gespielteKarte")) {
-            //e1 = spiel.getComputerAusgespielteKarte();
-            buttonE.setText(e1.getFarbe()+e1.getWertigkeit());
-        } else if (o.equals("Spielende")) {
-            DialogFragment newFragment = new GameEnd();
-            newFragment.show(getFragmentManager(), "Game End");
+            spiel.ZugAuswerten(k1, e1);
+            punkteAktualisieren();
+            e1 = null;
+            if (spiel.istSpielzuEnde(bummerl)) {
+                DialogFragment newFragment = new GameEnd();
+                newFragment.show(getFragmentManager(), "GameEnd");
+            }
         }
     }
+
+    private void zugWechsel() {
+        boolean temp = s1.isIstdran();
+        button1.setClickable(temp);
+        button2.setClickable(temp);
+        button3.setClickable(temp);
+        button4.setClickable(temp);
+        button5.setClickable(temp);
+        if (temp) {
+            i.setTextColor(0xff0000);
+            enemy.setTextColor(0x555555);
+        } else {
+            i.setTextColor(0x555555);
+            enemy.setTextColor(0xff0000);
+            button1.setClickable(false);
+            button2.setClickable(false);
+            button3.setClickable(false);
+            button4.setClickable(false);
+            button5.setClickable(false);
+            e1 = spiel.AuspielenComputer(null);
+            buttonE.setText(e1.getFarbe()+e1.getWertigkeit());
+            zugWechsel();
+        }
+    }
+
+    private void karteGezogen() {
+        //stapel = spiel.getStapel();
+        //buttonT.setText(stapel.size());
+        k1 = s1.Hand.get(0);
+        k2 = s1.Hand.get(1);
+        k3 = s1.Hand.get(2);
+        k4 = s1.Hand.get(3);
+        k5 = s1.Hand.get(4);
+        button1.setText(k1.getFarbe()+k1.getWertigkeit());
+        button2.setText(k2.getFarbe()+k2.getWertigkeit());
+        button3.setText(k3.getFarbe()+k3.getWertigkeit());
+        button4.setText(k4.getFarbe()+k4.getWertigkeit());
+        button5.setText(k5.getFarbe()+k5.getWertigkeit());
+    }
+
+    private void punkteAktualisieren() {
+        buttonI.setText("");
+        buttonE.setText("");
+        int p1 = s1.getPunkte();
+        int p2 = s2.getPunkte();
+        punkte.setText(p1+":"+p2);
+    }
+
+    private void gespielteKarteEntfernen() {
+        if (s1.Hand.get(0)==null) {
+            button1.setVisibility(View.INVISIBLE);
+        } else if (s1.Hand.get(1)==null) {
+            button2.setVisibility(View.INVISIBLE);
+        } else if (s1.Hand.get(2)==null) {
+            button3.setVisibility(View.INVISIBLE);
+        } else if (s1.Hand.get(3)==null) {
+            button4.setVisibility(View.INVISIBLE);
+        } else if (s1.Hand.get(4)==null) {
+            button5.setVisibility(View.INVISIBLE);
+        }
+    }
+
+        /*} else if (o.equals("Trumpfkarte")) {
+            //t = spiel.getAufgedeckterTrumpf();
+            buttonT.setText(t.getFarbe()+t.getWertigkeit());
+        }*/
 }
