8c1f834c81e55f14c537b9614997e0ccf7100fc3
frederikflpl <frederik.platter@hotmail.de>
2015-04-08 22:13:54 +0200

    GUI-Abrundung
    noch ein recommit

--- app/src/main/java/appsolutegamesgmbh/schnaps2gether/GUI/GameEnd.java
+++ app/src/main/java/appsolutegamesgmbh/schnaps2gether/GUI/GameEnd.java
@@ -1,5 +1,6 @@
 package appsolutegamesgmbh.schnaps2gether.GUI;
 
+import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.Dialog;
 import android.app.DialogFragment;
@@ -12,6 +13,32 @@ import appsolutegamesgmbh.schnaps2gether.R;
  * Created by Frederik on 06.04.2015.
  */
 public class GameEnd extends DialogFragment {
+
+    /* The activity that creates an instance of this dialog fragment must
+     * implement this interface in order to receive event callbacks.
+     * Each method passes the DialogFragment in case the host needs to query it. */
+    public interface GameEndDialogListener {
+        public void onDialogPositiveClick(DialogFragment dialog);
+        public void onDialogNegativeClick(DialogFragment dialog);
+    }
+
+    // Use this instance of the interface to deliver action events
+    GameEndDialogListener mListener;
+
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+        // Verify that the host activity implements the callback interface
+        try {
+            // Instantiate the NoticeDialogListener so we can send events to the host
+            mListener = (GameEndDialogListener) activity;
+        } catch (ClassCastException e) {
+            // The activity doesn't implement the interface, throw exception
+            throw new ClassCastException(activity.toString()
+                    + " must implement GameEndDialogListener");
+        }
+    }
+
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
         // Use the Builder class for convenient dialog construction
@@ -19,13 +46,14 @@ public class GameEnd extends DialogFragment {
         builder.setMessage(R.string.dialog_game_end)
                 .setPositiveButton(R.string.play_again, new DialogInterface.OnClickListener() {
                     public void onClick(DialogInterface dialog, int id) {
-                        // reset game and start it
+                        // Send the positive button event back to the host activity
+                        mListener.onDialogPositiveClick(GameEnd.this);
                     }
                 })
                 .setNegativeButton(R.string.exit, new DialogInterface.OnClickListener() {
                     public void onClick(DialogInterface dialog, int id) {
-                        // terminate the app
-                        getActivity().finish();
+                        // Send the negative button event back to the host activity
+                        mListener.onDialogNegativeClick(GameEnd.this);
                     }
                 });
         // Create the AlertDialog object and return it
