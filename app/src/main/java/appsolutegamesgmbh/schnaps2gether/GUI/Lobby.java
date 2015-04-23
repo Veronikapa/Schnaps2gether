c450e95c714847e224500c3baa4d664c6fd0a3d0
Veronikapa <v.pachatz@yahoo.com>
2015-04-24 00:33:55 +0200

    Beheben der Merge-Konflikte
    Merge Konflikt in Lobby.java mit Änderungen von Kerstin wurden behoben

--- app/src/main/java/appsolutegamesgmbh/schnaps2gether/GUI/Lobby.java
+++ app/src/main/java/appsolutegamesgmbh/schnaps2gether/GUI/Lobby.java
@@ -1,21 +1,15 @@
 package appsolutegamesgmbh.schnaps2gether.GUI;
 
 import android.app.Activity;
-<<<<<<< HEAD
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.IntentFilter;
 import android.net.wifi.p2p.WifiP2pManager;
-=======
 import android.content.Intent;
->>>>>>> origin/master
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-
-import java.nio.channels.Channel;
 
 import appsolutegamesgmbh.schnaps2gether.R;
 import appsolutegamesgmbh.schnaps2gether.W2P2.WP2PBroadCastReceiver;
@@ -69,7 +63,6 @@ public class Lobby extends Activity {
         return super.onOptionsItemSelected(item);
     }
 
-<<<<<<< HEAD
     //VP: Registrierung des broadcast receivers
     @Override
     protected void onResume() {
@@ -82,10 +75,9 @@ public class Lobby extends Activity {
     protected void onPause() {
         super.onPause();
         unregisterReceiver(w2p2BroadCastReceiver);
-=======
+    }
 
     public void neu(View v){
         Intent intent = new Intent(this, NeuesSpiel.class);
->>>>>>> origin/master
     }
 }
